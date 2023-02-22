a97cfb9c108689ab3e5f98fd96d4c963dc05ccf7
zagaouch <i.zagaouch@gmail.com>
2023-02-22 03:52:40 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,4 +7,10 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
+    public function home(){
+        return view('home');
+    }
+    public function about(){
+        return view('about');
+    }
 }
